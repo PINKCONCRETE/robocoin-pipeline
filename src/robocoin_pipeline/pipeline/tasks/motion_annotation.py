747324405fa156f68bdfb840d1aafcda77c90415
lxc <lxc@lxc.com>
2025-12-01 14:47:57 +0800

    Init dev

--- src/robocoin_pipeline/pipeline/tasks/motion_annotation.py
+++ src/robocoin_pipeline/pipeline/tasks/motion_annotation.py
@@ -1,9 +1,18 @@
+from pathlib import Path
+
+from robocoin_pipeline.utils.file_sync import sync_files
+
 from .task_registry import register_task
 
 
 @register_task(ncpu=1, nmem=4, ngpu=0)
 def motion_annotation(
-    repo_path: str, input_features: list[str], output_features: list[str]
+    repo_path: str | Path,
+    need_ori_data: bool,
+    input_features: list[str],
+    output_features: list[str] | None = None,
 ):
     """模拟Motion Annotation任务"""
+    sync_files(repo_path, need_ori_data)
+
     pass
---
+++ src/robocoin_pipeline/utils/file_sync.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+
+def sync_files(
+    repo_path: str | Path,
+    need_ori_data: bool = False,
+    input_features: list[str] = None,
+    output_features: list[str] = None,
+):
+    pass
