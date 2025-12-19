7d2ca003049b8ed7ffa346964b0da345fb31ae81
PINKCONCRETE <1552738325@qq.com>
2025-12-19 13:03:35 +0800

    docs: 防止上传本地文件

---
+++ src/robocoin_pipeline/pipeline/tasks/scene_annotation.py
@@ -0,0 +1,23 @@
+import time
+from logging import Logger, getLogger
+from pathlib import Path
+
+from robocoin_pipeline.pipeline.tasks.task_registry import (
+    register_single_input_field_task,
+)
+
+
+@register_single_input_field_task
+def scene_annotation(
+    repo_path: str | Path,
+    input_field: str,
+    output_field: str,
+    config: dict,
+    logger: Logger | None = None,
+) -> None:
+    if logger is None:
+        logger = getLogger(__name__)
+    """模拟Scene Annotation任务"""
+    logger.info("Begin running scene_annotation")
+    time.sleep(2)
+    logger.info("Finished running scene_annotation")
